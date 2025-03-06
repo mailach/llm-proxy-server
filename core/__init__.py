1d2d58548a3eecd73d15c66dcc5cc52189c82f76
mailach <alina.mailach@informatik.uni-leipzig.de>
2025-03-06 11:11:50 +0100

    Add flask-admin

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,12 @@
 import os
+import logging
 import json
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 import flask_login
 
 
@@ -42,26 +45,45 @@ def create_app():
         "SQLALCHEMY_DATABASE_URI"
     ] = f"postgresql://{PG_USER}:{PG_PW}@{PG_HOST}:{PG_PORT}/{PG_DB}"
     app.config['JSON_AS_ASCII'] = False
+    
 
     app.secret_key = os.environ.get("FLASK_SECRET_KEY")
     db.init_app(app)
     migrate.init_app(app, db)
-
     if PROXY_PATH_PREFIX:
         app.wsgi_app = ProxyFix(app.wsgi_app)
+        
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+    admin = Admin(app, name='LLM Proxy', template_mode='bootstrap3')
+    
+
+
 
     # Blueprint imports
     with app.app_context():
+        
+        
         from core import models
         from core.blueprints.root import root
         from core.blueprints.auth import auth
-        from core.blueprints.admin import admin
+        from core.models import User
+        
+        
+    class UserModelView(ModelView):
+        can_delete = False  # disable model deletion
+        page_size = 50 
+        can_view_details = True
+        column_list = (User.id, User.pw_hash)
+
+        
+    admin.add_view(UserModelView(User, db.session))
+        #from core.blueprints.admin import admin
 
     login_manager.init_app(app)
 
     # Register imported blueprints
     app.register_blueprint(root)
     app.register_blueprint(auth)
-    app.register_blueprint(admin)
+    #app.register_blueprint(admin)
 
     return app
