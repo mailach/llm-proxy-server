cc87bffeee185091e29f1f49f654d70418460c09
mailach <alina.mailach@informatik.uni-leipzig.de>
2025-03-12 11:22:46 +0100

    add data model for users with budget and language model pricing

--- core/models.py
+++ core/models.py
@@ -16,12 +16,27 @@ class DBClass:
         logging.info("Successfully deleted %r", self)
 
 
-# Beispiel Userklasse, muss entsprechend angepasst werden
 class User(flask_login.UserMixin, db.Model, DBClass):
     __tablename__ = "user"
-    id = db.Column(db.String(200), primary_key=True)
-    pw_hash = db.Column(db.String(200))
-
+    id = db.Column(db.String(400), primary_key=True)
+    pw_hash = db.Column(db.String(400))
+    budget = db.Column(db.Float)
+    used_budget = db.Column(db.Float)
+    api_key = db.Column(db.String(400))
 
     def __repr__(self):
-        return f"<User id={self.id} >"
+        return f"<User id={self.id}, budget={self.budget}, used_budget={self.used_budget}>"
+    
+    
+class LanguageModel(flask_login.UserMixin, db.Model, DBClass):
+    __tablename__ = "model"
+    name = db.Column(db.String(400), primary_key=True)
+    encoding_model = db.Column(db.String(400))
+    provider = db.Column(db.String(400))
+    price_input_token = db.Column(db.Float)
+    price_output_token = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"<LanguageModel name={self.name}, encoding_model={self.encoding_model}, provider={self.provider}>"
+    
+
